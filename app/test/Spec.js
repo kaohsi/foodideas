e6941b66f2305b43f3d8edb4ce98ce63729e695e
Richard Foong <richard.foong@twinspires.com>
2016-10-31 17:01:08 -0700

    Adding unit test file and bower.json

---
+++ app/test/Spec.js
@@ -0,0 +1,14 @@
+describe('foodIdeas.searchRestaurant', function() {
+
+  /*var SearchRestaurant;
+
+  beforeEach(module('foodIdeas'));
+  beforeEach(inject(function($injector) {
+    SearchRestaurant = $injector.get('searchRestaurant');
+  }));*/
+
+  it('First test', function() {
+    expect(true).toEqual(true);
+  });
+
+});
